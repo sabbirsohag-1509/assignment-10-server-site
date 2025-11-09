28ab9cdabaf721d6210dca0ad2049cc6cc1229d0
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-11-09 14:25:21 +0600

    post and get method implement

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,7 +13,51 @@ app.get('/', (req, res) => {
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mycluster.eyaxb6h.mongodb.net/?appName=myCluster`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+
+
+    const myDB = client.db("homeNest");
+    const propertiesCollection = myDB.collection("properties");
+
+    //POST
+    app.post("/properties", async (req, res) => {
+      const newProperty = req.body;
+      const result = await propertiesCollection.insertOne(newProperty);
+      res.send(result);
+    });
+
+    //GET
+    app.get("/properties", async (req, res) => {
+      const result = await propertiesCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
