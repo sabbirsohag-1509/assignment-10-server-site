a932be8eb10f79349a149ca404a6ec138fed572a
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-11-10 23:19:56 +0600

    after details pathname change

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ async function run() {
       res.send(result);
     });
 
+    //Delete
+    
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
